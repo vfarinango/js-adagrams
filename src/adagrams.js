98bd7b1cab4f9f647f89c2895646810a29b4ec72
Vicky Farinango <vickyfarinango@gmail.com>
2025-06-01 23:58:59 -0500

    feat: Implements Wave 1. Adds a helper function createLetterPool() which Generates the initial letter pool with specified letter distribution. It is used in drawLetters().

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,35 @@
+export const createLetterPool = () => {
+  const LETTERPOOL = {
+      "A":9,"B":2,"C":2,"D":4,
+      "E":12,"F":2,"G":3,"H":2,
+      "I":9,"J":1,"K":1,"L":4,"M":2,
+      "N":6,"O":8,"P":2,"Q":1,
+      "R":6,"S":4,"T":6,"U":4,
+      "V":2,"W":2,"X":1,
+      "Y":2,"Z":1
+  };
+
+  let letterList = [];
+  for (const [key,value] of Object.entries(LETTERPOOL)) {
+    for (let i = 0; i < value; i++) {
+      letterList.push(key);
+    };
+  };
+  return letterList;
+};
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  let letterPool = createLetterPool();
+
+  let letterHand = [];
+  for (let i =0; i < 10; i++) {
+    let randomIndex = Math.floor(Math.random() * letterPool.length);
+    let randomLetter = letterPool[randomIndex];
+    letterHand.push(randomLetter);
+    letterPool.splice(randomIndex,1);
+  };
+
+  return letterHand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
--- test/adagrams.test.js
+++ test/adagrams.test.js
@@ -70,6 +70,7 @@ describe("Adagrams", () => {
     });
   });
 
+  
   describe("usesAvailableLetters", () => {
     it("returns true if the submitted letters are valid against the drawn letters", () => {
       const drawn = ["D", "O", "G", "X", "X", "X", "X", "X", "X", "X"];
